2226a3b127983569ac602fd0f07f643a8fdf00d9
srihastut <149176809+srihastut@users.noreply.github.com>
2024-11-11 20:37:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ window.onscroll = () => {
 };
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/Sri-Portofolio/service-worker.js')
       .then(registration => {
         console.log('Service Worker registered with scope:', registration.scope);
   
